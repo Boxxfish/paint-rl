0dfd386d3f7049d49ff8fc5449f01573bac15c21
Ben Giacalone <bensgiacalone@gmail.com>
2023-06-11 18:33:43 -0400

    Make strokes smaller

--- paint_rl/experiments/supervised_strokes/gen_supervised.py
+++ paint_rl/experiments/supervised_strokes/gen_supervised.py
@@ -2,11 +2,12 @@
 Generates the dataset for the supervised setting.
 """
 import json
+import math
 import os
 import random
 import shutil
 from pathlib import Path
-from typing import List, Tuple
+from typing import List, Optional, Tuple
 
 import bezier  # type: ignore
 import numpy as np
@@ -14,6 +15,8 @@ from PIL import Image, ImageDraw  # type: ignore
 from tqdm import tqdm
 
 IMG_SIZE = 64
+MIN_DIST = 8
+MAX_DIST = 32
 NUM_IMAGES = 1000
 
 CANVAS_COLOR = (255, 0, 0)
@@ -22,10 +25,15 @@ CANVAS_TARGET_COLOR = (255, 255, 0)
 PEN_POS_COLOR = (0, 0, 255)
 
 
-def rand_point(img_size: int) -> Tuple[int, int]:
-    x = int(random.random() * img_size)
-    y = int(random.random() * img_size)
-    return (x, y)
+def rand_point(min_: int, max_: int, prev: Optional[Tuple[int, int]]) -> Tuple[int, int]:
+    dist = min_ + random.random() * (max_ - min_)
+    angle = random.random() * 2.0 * math.pi
+    x = 0
+    y = 0
+    if prev:
+        x = prev[0]
+        y = prev[1]
+    return (clip(x + int(dist * math.cos(angle)), 0, IMG_SIZE), clip(0, y + int(dist * math.sin(angle)), IMG_SIZE))
 
 
 def gen_curve_points(
@@ -43,6 +51,8 @@ def gen_curve_points(
     points.append((point[0], point[1]))
     return points
 
+def clip(val: int, min_: int, max_: int):
+    return min(max(val, min_), max_)
 
 def gen_sample(img_size: int) -> Tuple[Image.Image, List[Tuple[int, int]]]:
     c_img = Image.new("1", (img_size, img_size))
@@ -51,30 +61,34 @@ def gen_sample(img_size: int) -> Tuple[Image.Image, List[Tuple[int, int]]]:
     r_draw = ImageDraw.Draw(r_img)
     num_before = random.randrange(0, 2)
     num_after = random.randrange(0, 2)
-    lines_before = [rand_point(img_size) for _ in range(num_before + 1)]
+    lines_before = [rand_point(MIN_DIST, MAX_DIST, prev=(IMG_SIZE // 2, IMG_SIZE // 2))]
+    for i in range(1, num_before + 1):
+        lines_before.append(rand_point(MIN_DIST, MAX_DIST, prev=lines_before[i - 1]))
     last_point = lines_before[-1]
-    mid_point = rand_point(img_size)
-    next_point = rand_point(img_size)
+    mid_point = rand_point(MIN_DIST, MAX_DIST, prev=last_point)
+    next_point = rand_point(MIN_DIST, MAX_DIST, prev=last_point)
     path = [last_point, mid_point, next_point]
     points = gen_curve_points(last_point, mid_point, next_point)
     r_draw.line(points, 1, width=1)
-    lines_after = [next_point] + [rand_point(img_size) for _ in range(num_after)]
+    lines_after = [next_point]
+    for i in range(num_after):
+        lines_after.append(rand_point(MIN_DIST, MAX_DIST, prev=lines_after[i - 1]))
     prev_point_stroke = lines_before[0]
     for i in range(len(lines_before) - 1):
         prev_point = lines_before[i]
         next_point = lines_before[i + 1]
-        mid = rand_point(img_size)
+        mid = rand_point(MIN_DIST, MAX_DIST, prev=prev_point)
         points = gen_curve_points(prev_point, mid, next_point)
         r_draw.line(points, 1, width=1)
-        next_point_stroke = (next_point[0] + random.randrange(-4, 4), next_point[1] + random.randrange(-4, 4))
-        points = gen_curve_points(prev_point_stroke, (mid[0] + random.randrange(-4, 4), mid[1] + random.randrange(-4, 4)), next_point_stroke)
+        next_point_stroke = (next_point[0] + random.randrange(-2, 2), next_point[1] + random.randrange(-2, 2))
+        points = gen_curve_points(prev_point_stroke, (mid[0] + random.randrange(-2, 2), mid[1] + random.randrange(-2, 2)), next_point_stroke)
         c_draw.line(points, 1, width=1)
         prev_point_stroke = next_point_stroke
         last_point = (max(min(int(prev_point_stroke[0]), img_size - 1), 0), max(min(int(prev_point_stroke[1]), img_size - 1), 0))
     for i in range(len(lines_after) - 1):
         prev_point = lines_after[i]
         next_point = lines_after[i + 1]
-        mid = rand_point(img_size)
+        mid = rand_point(MIN_DIST, MAX_DIST, prev=prev_point)
         points = gen_curve_points(prev_point, mid, next_point)
         r_draw.line(points, 1, width=1)
     pos_layer_x = np.abs(np.arange(0 - last_point[0], img_size - last_point[0]))[np.newaxis, ...].repeat(img_size, 0) / img_size
--- paint_rl/experiments/supervised_strokes/stroke_env.py
+++ paint_rl/experiments/supervised_strokes/stroke_env.py
@@ -2,13 +2,19 @@ import random
 from typing import Any, Optional
 import gymnasium as gym
 import numpy as np
-from PIL import Image, ImageDraw #type: ignore
+from PIL import Image, ImageDraw  # type: ignore
 import pygame
-from scipy.ndimage import gaussian_filter #type: ignore
+from scipy.ndimage import gaussian_filter  # type: ignore
 
-from paint_rl.experiments.supervised_strokes.gen_supervised import gen_curve_points, rand_point
+from paint_rl.experiments.supervised_strokes.gen_supervised import (
+    MAX_DIST,
+    MIN_DIST,
+    gen_curve_points,
+    rand_point,
+)
 
 SCREEN_SCALE = 4
+
 
 class StrokeEnv(gym.Env):
     """
@@ -40,9 +46,10 @@ class StrokeEnv(gym.Env):
         self.correct_moves: list[tuple[tuple[float, float], tuple[float, float]]] = []
         if self.render_mode == "human":
             pygame.init()
-            self.screen = pygame.display.set_mode((img_size * SCREEN_SCALE, img_size * SCREEN_SCALE))
+            self.screen = pygame.display.set_mode(
+                (img_size * SCREEN_SCALE, img_size * SCREEN_SCALE)
+            )
             self.clock = pygame.time.Clock()
-
 
     def step(
         self, action: np.ndarray
@@ -63,12 +70,16 @@ class StrokeEnv(gym.Env):
         self.counter += 1
         trunc = self.counter == self.num_strokes
 
-        pixels_moved = abs(self.last_pos[0] - end_point[0]) + abs(self.last_pos[1] - end_point[1])
+        pixels_moved = abs(self.last_pos[0] - end_point[0]) + abs(
+            self.last_pos[1] - end_point[1]
+        )
         if pixels_moved <= 4:
             reward -= 0.1
         self.last_pos = end_point
 
-        pos_channel = self.gen_pos_channel(self.last_pos[0], self.last_pos[1], self.img_size)
+        pos_channel = self.gen_pos_channel(
+            self.last_pos[0], self.last_pos[1], self.img_size
+        )
         obs = np.stack([self.canvas, self.ref, pos_channel])
 
         return obs, reward, done, trunc, {}
@@ -78,21 +89,31 @@ class StrokeEnv(gym.Env):
         draw = ImageDraw.Draw(img)
         self.num_strokes = random.randrange(1, 4)
         self.counter = 0
-        self.last_pos = rand_point(self.img_size)
+        self.last_pos = rand_point(
+            MIN_DIST, MAX_DIST, prev=(self.img_size // 2, self.img_size // 2)
+        )
         curr_pos = self.last_pos
         self.correct_moves = []
         for _ in range(self.num_strokes):
-            mid_point = rand_point(self.img_size)
-            end_point = rand_point(self.img_size)
+            mid_point = rand_point(
+                MIN_DIST, MAX_DIST, prev=(self.img_size // 2, self.img_size // 2)
+            )
+            end_point = rand_point(
+                MIN_DIST, MAX_DIST, prev=(self.img_size // 2, self.img_size // 2)
+            )
             self.correct_moves.append((mid_point, end_point))
             points = gen_curve_points(curr_pos, mid_point, end_point)
             draw.line(points, 1, 1)
             curr_pos = end_point
         self.ref = np.array(img).swapaxes(0, 1)
-        self.ref_filter = (self.ref + gaussian_filter(np.float32(self.ref), sigma=16) * 4).clip(0, 1)
+        self.ref_filter = (
+            self.ref + gaussian_filter(np.float32(self.ref), sigma=4) * 2
+        ).clip(0, 1)
 
         self.canvas = np.zeros([self.img_size, self.img_size])
-        pos_channel = self.gen_pos_channel(self.last_pos[0], self.last_pos[1], self.img_size)
+        pos_channel = self.gen_pos_channel(
+            self.last_pos[0], self.last_pos[1], self.img_size
+        )
         obs = np.stack([self.canvas, self.ref, pos_channel])
         self.canvas_draw.rectangle((0, 0, self.img_size, self.img_size), 0)
         self.last_diff = (np.abs(self.canvas - self.ref_filter)).sum() / self.max_diff
@@ -102,13 +123,28 @@ class StrokeEnv(gym.Env):
         if self.render_mode == "human":
             pos_channel = np.zeros([self.img_size, self.img_size])
             pos_channel[self.last_pos[0]][self.last_pos[1]] = 1
-            img = np.stack([self.canvas, self.ref_filter, pos_channel]).swapaxes(0, 2) * 255.0
+            img = (
+                np.stack([self.canvas, self.ref_filter, pos_channel]).swapaxes(0, 2)
+                * 255.0
+            )
             img_surf = pygame.surfarray.make_surface(img)
-            pygame.transform.scale(img_surf, (self.img_size * SCREEN_SCALE, self.img_size * SCREEN_SCALE), self.screen)
+            pygame.transform.scale(
+                img_surf,
+                (self.img_size * SCREEN_SCALE, self.img_size * SCREEN_SCALE),
+                self.screen,
+            )
             pygame.display.flip()
             self.clock.tick(20)
 
     def gen_pos_channel(self, x: int, y: int, img_size: int) -> np.ndarray:
-        pos_layer_x = np.abs(np.arange(0 - x, img_size - x))[np.newaxis, ...].repeat(img_size, 0) / img_size
-        pos_layer_y = np.abs(np.arange(0 - y, img_size - y))[np.newaxis, ...].repeat(img_size, 0).T / img_size
+        pos_layer_x = (
+            np.abs(np.arange(0 - x, img_size - x))[np.newaxis, ...].repeat(img_size, 0)
+            / img_size
+        )
+        pos_layer_y = (
+            np.abs(np.arange(0 - y, img_size - y))[np.newaxis, ...]
+            .repeat(img_size, 0)
+            .T
+            / img_size
+        )
         return np.sqrt(pos_layer_x**2 + pos_layer_y**2)
